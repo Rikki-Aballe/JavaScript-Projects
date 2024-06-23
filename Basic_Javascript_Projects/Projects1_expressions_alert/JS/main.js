cd3c3080f55a355c865db9634d8664e19ac89fdb
Rikki-Aballe <rikkiaballe02@gmail.com>
2024-06-23 14:55:55 -0300

    created javascript file

---
+++ Basic_Javascript_Projects/Projects1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+var a="The quick brown fox jumps over the lazy dog while the cat sleeps peacefully nearby"
+window.alert(a)//windows.alert method
+var part1 = "The journey of a thousand miles", part2 = " begins with", part3 = " a single step.", author = " - Lao Tzu"
+
+var quote = part1 + part2 + "\n" + part3 + author + "\n";
+document.write(quote)//concatenate
+document.write(71-2)//expression
+var sent1="This is the beginning of the string"
+var sent2="and this is the end of the string"
+document.write(sent1 + "\n" + sent2)
